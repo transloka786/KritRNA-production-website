feb12581c7a64716b1a5a22966437281b1e6179a
transloka786 <trnaativetransloka@gmail.com>
2026-04-08 16:50:31 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -32,6 +32,6 @@ export default function robots(): MetadataRoute.Robots {
         allow: '/',
       },
     ],
-    sitemap: 'https://hellokritna.com/sitemap.xml',
+    sitemap: 'https://hellokritrna.com/sitemap.xml',
   }
 }
